6734d8222748ac73352a736352b808fc96a158cf
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-07-15 21:15:04 +0200

    Update ErrorView.kt

--- gorillas/src/main/kotlin/br/com/schumaker/gorillas/common/ErrorView.kt
+++ gorillas/src/main/kotlin/br/com/schumaker/gorillas/common/ErrorView.kt
@@ -2,6 +2,9 @@ package br.com.schumaker.gorillas.common
 
 import java.time.LocalDateTime
 
+/**
+ * @author Hudson Schumaker
+ */
 data class ErrorView(
     val timestamp: LocalDateTime = LocalDateTime.now(),
     val status: Int,
