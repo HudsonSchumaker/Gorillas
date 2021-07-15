3c5c8faf56627747fe7818eefa090b6e8045b091
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-07-15 21:16:40 +0200

    Update UserRepository.kt

--- gorillas/src/main/kotlin/br/com/schumaker/gorillas/user/model/repository/UserRepository.kt
+++ gorillas/src/main/kotlin/br/com/schumaker/gorillas/user/model/repository/UserRepository.kt
@@ -5,6 +5,9 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Mono
 
+/**
+ * @author Hudson Schumaker
+ */
 @Repository
 interface UserRepository : ReactiveCrudRepository<User, Long> {
 
